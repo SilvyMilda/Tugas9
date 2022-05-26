797c0581da39831c742d60a3f3f445a36b5c5862
SilvyMilda <100079356+SilvyMilda@users.noreply.github.com>
2022-05-26 09:55:12 +0700

    Add files via upload

---
+++ p-index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORM LOGIN</title>	
+</head>
+<body>
+	<h1>FORM LOGIN MODIFIKASI</h1>
+	<h3>By: Silvy Milda Puspita</h3>
+	<form action="cetak2.php" method="post">		
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="login" value="Log In"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
